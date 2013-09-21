5d25f0434f7206547d83b909c702d044b777d130
SebastianJay <sebastianjay2@gmail.com>
2013-09-21 01:49:40 -0400

    Update Character.cs
    Nothing like late night coding.
    Has animation methods.

--- Character.cs
+++ Character.cs
@@ -2,10 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
 
 namespace SunsetHigh    //adjust as needed
 {
+    /*
+     * Specifies which direction a character is facing.
+     */
+    public enum Direction                       
+    {
+        North = 0,
+        East,
+        South,
+        West
+    };
+    
     /*
      * Every character has a sprite drawing box and collision box (which are the same, size-wise).
      * 
@@ -14,29 +27,27 @@ namespace SunsetHigh    //adjust as needed
      */
     public class Character
     {
-        public static enum Direction                //used to specify where characters are facing
-        {
-            North = 0,
-            East,
-            South,
-            West
-        };   
-
-        private static const int ACTION_OFFSET = 5; //pixels offset between sprite and action boxes
+        private const int ACTION_OFFSET = 5;        //pixels offset between sprite and action boxes
                                                     //the "action box" being area where character can interact with environment                                       
-
-        private int spriteX, spriteY;               //top-left corner of sprite and collision box
+        //geometry
+        private int spriteX, spriteY;                   //top-left corner of sprite and collision box
         private int spriteWidth, spriteHeight;      //size of sprite, collision box
         private int direction;                      //which direction the character is facing
+        private bool moving;
+        //sprite image
         private Texture2D image;                    //the picture or spritesheet to draw
         private int imageRows, imageColumns;        //rows and columns in spritesheet
-        //private int animationSpeed;                 //speed at which to animate sprite
+        private int frameColumn;                       //the current frame in the sheet we are drawing
+        private int frameRow;                       //the current row of the sheet we are drawing
+        private float animationTime;               //speed at which to animate sprite
+        private float totalElapsed;                 //personal timer for animation purposes
         //private bool visible;                       //sprite visibility
+        //misc
         private bool male;                          //male or female
         //private Dialogue script;                  //script given to NPCs
 
         //
-        //Some constructors here
+        //Constructors go here
         //
 
         public int getX() { return this.spriteX; }
@@ -52,19 +63,39 @@ namespace SunsetHigh    //adjust as needed
             this.spriteX = x;
             this.spriteY = y;
         }
+        public void setDimensions(int width, int height)
+        {
+            this.spriteWidth = width;
+            this.spriteHeight = height;
+        }
         public void setDirection(int dir)
         {
             if(dir >= 0 && dir < 4)
-                direction = dir;
+                this.direction = dir;
         }
-        
+        public void loadImage(ContentManager content, string fileName, int numRows, int numCols, float anTime)
+        {
+            this.image = content.Load<Texture2D>(fileName);
+            this.frameRow = 0;
+            this.frameColumn = 0;
+            this.imageRows = numRows;
+            this.imageColumns = numCols;
+            this.animationTime = anTime;
+        }
+
+        /*
+         * Handles 1D movement
+         */
         public void move(int dir, int speed)
         {
-            setDirection(dir);
+            this.setDirection(dir);
             //other stuff
         }
         
-        public void move2D(int dir1, int dir2, int speed1, int speed2) //for moving diagonally
+        /*
+         * Handles moving diagonally
+         */
+        public void move2D(int dir1, int dir2, int speed1, int speed2)
         {
         }
 
@@ -97,6 +128,38 @@ namespace SunsetHigh    //adjust as needed
         public bool bothFacing(Character other)
         {
             return this.facing(other) && other.facing(this);
+        }
+
+
+        /*
+         * Updates which frame on sprite sheet to draw
+         * Adapted from MSDN XNA tutorials
+         */
+        public void updateFrame(float elapsed)
+        {
+            this.totalElapsed += elapsed;
+            if (this.totalElapsed > this.animationTime)
+            {
+                this.frameColumn++;
+                this.frameColumn = this.frameColumn % this.imageColumns;
+                this.totalElapsed -= this.animationTime;
+            }
+        }
+
+        /*
+         * Draws itself!
+         * Adapted from MSDN XNA tutorials
+         */
+        public void draw(SpriteBatch sb)
+        {
+            int frameWidth = this.image.Width / this.imageColumns;
+            int frameHeight = this.image.Height / this.imageRows;
+            Rectangle sourceRect = new Rectangle(frameWidth * frameColumn, 0,
+                frameWidth, frameHeight);
+            Rectangle mapPosRect = new Rectangle(this.getX(), this.getY(), 
+                this.getWidth(), this.getHeight());
+            sb.Draw(this.image, mapPosRect, sourceRect, Color.White);
+                //more features available in sb.Draw(...);
         }
     }
 }
