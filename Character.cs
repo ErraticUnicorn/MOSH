10c5cd4585b19ab9d9314b6bd0e3f3c1cba07676
SebastianJay <sebastianjay2@gmail.com>
2013-09-22 13:02:18 -0400

    Update Character.cs

--- Character.cs
+++ Character.cs
@@ -13,6 +13,7 @@ namespace SunsetHigh    //adjust as needed
      */
     public enum Direction                       
     {
+        Undefined = -1,
         North = 0,
         East,
         South,
@@ -23,7 +24,7 @@ namespace SunsetHigh    //adjust as needed
      * Every character has a sprite drawing box and collision box (which are the same, size-wise).
      * 
      * Every character also contains a sprite atlas and information about what drawing/animation to draw
-     * dependeing on the situation.
+     * depending on the situation.
      */
     public class Character
     {
@@ -32,7 +33,7 @@ namespace SunsetHigh    //adjust as needed
         //geometry
         private int spriteX, spriteY;                   //top-left corner of sprite and collision box
         private int spriteWidth, spriteHeight;      //size of sprite, collision box
-        private int direction;                      //which direction the character is facing
+        private Direction direction;                      //which direction the character is facing
         private bool moving;
         //sprite image
         private Texture2D image;                    //the picture or spritesheet to draw
@@ -42,19 +43,25 @@ namespace SunsetHigh    //adjust as needed
         private float animationTime;               //speed at which to animate sprite
         private float totalElapsed;                 //personal timer for animation purposes
         //private bool visible;                       //sprite visibility
-        //misc
+        //personal data
         private bool male;                          //male or female
+        private string name;                      //character's name
         //private Dialogue script;                  //script given to NPCs
 
-        //
-        //Constructors go here
-        //
+        public Character() { }                      //everything is zero/null
+        public Character(int x, int y, int w, int h)
+        {
+            spriteX = x;
+            spriteY = y;
+            spriteWidth = w;
+            spriteHeight = h;
+        }
 
         public int getX() { return this.spriteX; }
         public int getY() { return this.spriteY; }
         public int getWidth() { return this.spriteWidth; }
         public int getHeight() { return this.spriteHeight; }
-        public int getDirection() { return this.direction; }
+        public Direction getDirection() { return this.direction; }
         public bool isMale() { return this.male; }
         public bool isFemale() { return !this.male; }
 
@@ -68,10 +75,27 @@ namespace SunsetHigh    //adjust as needed
             this.spriteWidth = width;
             this.spriteHeight = height;
         }
-        public void setDirection(int dir)
+        public void setDirection(Direction dir)
         {
-            if(dir >= 0 && dir < 4)
-                this.direction = dir;
+            if (dir.Equals(Direction.Undefined)) return;
+
+            this.direction = dir;
+            //we should have a standard convention for spritesheets
+            //i.e. each row is a direction
+            if (this.getDirection().Equals(Direction.North))
+                this.frameRow = 3;
+            if (this.getDirection().Equals(Direction.East))
+                this.frameRow = 2;
+            if (this.getDirection().Equals(Direction.South))
+                this.frameRow = 0;
+            if (this.getDirection().Equals(Direction.West))
+                this.frameRow = 1;
+
+        }
+        public void setPersonalData(bool male, string name)
+        {
+            this.male = male;
+            this.name = name;
         }
         public void loadImage(ContentManager content, string fileName, int numRows, int numCols, float anTime)
         {
@@ -86,19 +110,39 @@ namespace SunsetHigh    //adjust as needed
         /*
          * Handles 1D movement
          */
-        public void move(int dir, int speed)
+        public void move(Direction dir, int speed)
         {
+            this.moving = true;
             this.setDirection(dir);
-            //other stuff
+            if (dir.Equals(Direction.North))
+                spriteY -= speed;
+            if (dir.Equals(Direction.South))
+                spriteY += speed;
+            if (dir.Equals(Direction.East))
+                spriteX += speed;
+            if (dir.Equals(Direction.West))
+                spriteX -= speed;
         }
         
         /*
          * Handles moving diagonally
          */
-        public void move2D(int dir1, int dir2, int speed1, int speed2)
+        public void move2D(Direction dir1, Direction dir2, int speed1, int speed2)
         {
+            this.moving = true;
+            this.move(dir1, speed1);
+            this.move(dir2, speed2);
+            if (speed1 >= speed2) this.setDirection(dir1);
+            else this.setDirection(dir2);
         }
 
+        /*
+         * Stops moving
+         */
+        public void stop()
+        {
+            this.moving = false;
+        }
 
         /*
          * Checks if this character is close enough with another character to perform any action
@@ -137,12 +181,15 @@ namespace SunsetHigh    //adjust as needed
          */
         public void updateFrame(float elapsed)
         {
-            this.totalElapsed += elapsed;
-            if (this.totalElapsed > this.animationTime)
+            if (this.moving)
             {
-                this.frameColumn++;
-                this.frameColumn = this.frameColumn % this.imageColumns;
-                this.totalElapsed -= this.animationTime;
+                this.totalElapsed += elapsed;
+                if (this.totalElapsed > this.animationTime)
+                {
+                    this.frameColumn++;
+                    this.frameColumn = this.frameColumn % this.imageColumns;
+                    this.totalElapsed %= this.animationTime;
+                }
             }
         }
 
@@ -152,10 +199,13 @@ namespace SunsetHigh    //adjust as needed
          */
         public void draw(SpriteBatch sb)
         {
+            if (image == null)
+                return;
+
             int frameWidth = this.image.Width / this.imageColumns;
             int frameHeight = this.image.Height / this.imageRows;
-            Rectangle sourceRect = new Rectangle(frameWidth * frameColumn, 0,
-                frameWidth, frameHeight);
+            Rectangle sourceRect = new Rectangle(frameWidth * frameColumn, 
+                frameHeight * frameRow, frameWidth, frameHeight);
             Rectangle mapPosRect = new Rectangle(this.getX(), this.getY(), 
                 this.getWidth(), this.getHeight());
             sb.Draw(this.image, mapPosRect, sourceRect, Color.White);
