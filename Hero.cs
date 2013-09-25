8cb367b001cae74db63f5b4839e4aeb01f3baa16
SebastianJay <sebastianjay2@gmail.com>
2013-09-25 15:35:32 -0400

    Create Hero.cs

---
+++ Hero.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SunsetHigh
+{
+    class Hero : Character
+    {
+        //make this a singleton?
+
+        public Hero()
+            : base()
+        {
+        }
+
+        public Hero(int x, int y, int w, int h)
+            : base(x, y, w, h)
+        {
+        }
+
+        public void converse(Character c)
+        {
+        }
+
+        public void pickpocket(Character c) //need content manager
+        {
+            //make bar and needle pop up on screen
+        }
+    }
+}
